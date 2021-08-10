58dab85592b1854ef8e897ae9b901acffbba8f0c
navya2006 <navya2006.github.com>
2021-08-10 19:57:09 +0530

    bruhh

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var myFullpage = new fullpage('#fullpage', {
     navigation: true,
     scrollOverflow: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About Us'],
+    navigationTooltips: ['Home', 'About'],
     css3: false
 });
