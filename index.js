23c953b22833dd23bb34b4012521cdaa48a814ff
navya2006 <navya2006.github.com>
2021-08-11 08:24:39 +0530

    about section

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var myFullpage = new fullpage('#fullpage', {
     navigation: true,
     scrollOverflow: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About'],
+    navigationTooltips: ['Home', 'About', 'Section'],
     css3: false
 });
