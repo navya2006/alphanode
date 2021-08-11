8279b119ffbf22bba9f3e1886af985d8070bccf9
navya2006 <navya2006.github.com>
2021-08-11 14:31:38 +0530

    the problem

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var myFullpage = new fullpage('#fullpage', {
     navigation: true,
     scrollOverflow: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About', 'Section'],
+    navigationTooltips: ['Home', 'About', 'Problem'],
     css3: false
 });
