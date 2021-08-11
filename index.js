fbbc901bd437e16be4ac4394fa452296cd6ddeca
navya2006 <navya2006.github.com>
2021-08-11 15:04:13 +0530

    solution

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var myFullpage = new fullpage('#fullpage', {
     navigation: true,
     scrollOverflow: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About', 'Problem'],
+    navigationTooltips: ['Home', 'About Spaceffic', 'The Problem', 'Our Solution'],
     css3: false
 });
