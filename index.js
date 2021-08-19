73951923afd02a2c571def15e53084f83318a30e
navya2006 <navya2006.github.com>
2021-08-20 01:00:20 +0530

    uh-

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var myFullpage = new fullpage('#fullpage', {
     navigation: true,
     scrollOverflow: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About Spaceffic', 'The Problem', 'Our Solution', 'Teaser','Our Team'],
+    navigationTooltips: ['Home', 'About Spaceffic', 'The Problem', 'Our Solution', 'Teaser',
+     'Our Team'],
     css3: false
 });
 
